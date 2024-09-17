f97816d2721a6a08ee8203ac7ec62c703308d093
Alim1899 <alimakhchanogli@gmail.com>
2024-09-17 17:56:26 +0400

    Added add listing page

--- src/components/pages/addListing/AddListing.js
+++ src/components/pages/addListing/AddListing.js
@@ -1,10 +1,262 @@
+import { useEffect, useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import {
+  validationSchema,
+  initialValues,
+  fetchData,
+  submitForm,
+  changeHandler,
+} from "./assets";
+import add from "../../../assets/icons/add.svg";
+import classes from "./AddListing.module.css";
 const AddListing = () => {
-    
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [regions, setRegions] = useState([]);
+  const [cities, setCities] = useState([]);
+  const [selectedRegionId, setSelectedRegionId] = useState(localStorage.getItem('selectedRegionId')||'');
 
-export default AddListing
+  useEffect(() => {
+    fetchData(setRegions, setCities);
+  }, []);
+
+  return (
+    <div className={classes.main}>
+      <h2 className={classes.header}>ლისტინგის დამატება</h2>
+      <Formik
+        validateOnChange
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+      >
+        {(formik) => (
+          <Form
+            className={classes.form}
+            onSubmit={() => submitForm(formik.errors)}
+            onChange={(e) => changeHandler(e, formik.values.image)}
+          >
+            <div className={classes.type}>
+              <h2 className={classes.fieldHead}>გარიგების ტიპი:</h2>
+              <Field type="radio" name="type" value="იყიდება" />
+              <span className={classes.saleType}>იყიდება</span>
+
+              <Field type="radio" name="type" value="ქირავდება" />
+              <span className={classes.saleType}>ქირავდება</span>
+              <ErrorMessage
+                name="type"
+                component="div"
+                className={classes.error}
+              />
+            </div>
+
+            <div className={classes.address}>
+              <h2 className={classes.fieldHead}>მდებარეობა</h2>
+              <div className={classes.location}>
+                <label htmlFor="address">
+                  მისამართი *<Field type="text" name="address"></Field>
+                  <ErrorMessage
+                    name="address"
+                    component="div"
+                    className={classes.error}
+                  />{" "}
+                </label>
+                <label htmlFor="postal">
+                  საფოსტო ინდექსი *<Field type="text" name="postal"></Field>
+                  <ErrorMessage
+                    name="postal"
+                    component="div"
+                    className={classes.error}
+                  />
+                </label>
+              </div>
+
+              <div className={classes.region}>
+                <label htmlFor="region">
+                  რეგიონი
+                  <Field
+                    onChange={(e) => {
+                      const selectedRegionId = e.target.selectedOptions[0].id;
+                      setSelectedRegionId(selectedRegionId);
+                      formik.values.city = "";
+                      formik.handleChange(e);
+                    }}
+                    as="select"
+                    name="region"
+                    value={formik.values.region}
+                  >
+                    <option value="" disabled>
+                      აირჩიე რეგიონი
+                    </option>
+                    {regions.map((el) => {
+                      return (
+                        <option id={el.id} key={el.id}>
+                          {el.name}
+                        </option>
+                      );
+                    })}
+                  </Field>
+                  <ErrorMessage
+                    name="region"
+                    component="div"
+                    className={classes.error}
+                  />
+                </label>
+
+                <label htmlFor="city">
+                  ქალაქი
+                  <Field as="select" name="city" value={formik.values.city}>
+                    <option value="" disabled>
+                      აირჩიე ქალაქი
+                    </option>
+                    {cities.map((el) => {
+                      return el.region_id === Number(selectedRegionId) ? (
+                        <option id={el.region_id} key={el.id}>
+                          {el.name}
+                        </option>
+                      ) : null;
+                    })}
+                  </Field>
+                  <ErrorMessage
+                    name="city"
+                    component="div"
+                    className={classes.error}
+                  />
+                </label>
+              </div>
+            </div>
+
+            <div className={classes.details}>
+              <h2 className={classes.fieldHead}>ბინის დეტალები</h2>
+              <div className={classes.about}>
+                <label htmlFor="price">
+                  ფასი
+                  <Field type="text" name="price" value={formik.values.price} />
+                  <ErrorMessage
+                    name="price"
+                    component="div"
+                    className={classes.error}
+                  />
+                </label>
+                <label htmlFor="area">
+                  ფართობი
+                  <Field type="text" name="area" value={formik.values.area} />
+                  <ErrorMessage
+                    name="area"
+                    component="div"
+                    className={classes.error}
+                  />
+                </label>
+                <label htmlFor="bedrooms">
+                  საძინებლების რაოდენობა*
+                  <Field
+                    type="text"
+                    name="bedrooms"
+                    value={formik.values.bedrooms}
+                  />
+                  <ErrorMessage
+                    name="bedrooms"
+                    component="div"
+                    className={classes.error}
+                  />
+                </label>
+              </div>
+              <div className={classes.description}>
+                <label>
+                  აღწერა
+                  <Field
+                    type="text"
+                    name="description"
+                    values={formik.values.description}
+                  />
+                  <ErrorMessage
+                    name="description"
+                    component="div"
+                    className={classes.error}
+                  />
+                </label>
+              </div>
+            </div>
+
+            <div className={classes.photoInput}>
+              <label htmlFor="listingImage">ატვირთეთ ფოტო:</label>
+              <div className={classes.photoInputField}>
+                <Field
+                  id="fileInput"
+                  name="listingImage"
+                  type="file"
+                  accept="image/*"
+                  onChange={(event) => {
+                    const file = event.currentTarget.files[0];
+                    const maxSize = 1024 * 1024;
+                    if (file.size > maxSize) {
+                      alert("მაქსომალური ასატვირთი ზომა არის 1მბ");
+                      event.currentTarget.value = "";
+                      return;
+                    }
+
+                    if (file && file.type.startsWith("image/")) {
+                      const reader = new FileReader();
+                      reader.onload = () => {
+                        const imageURL = reader.result;
+                        formik.setFieldValue("image", imageURL);
+                        localStorage.setItem("listingImage", imageURL);
+                      };
+
+                      reader.readAsDataURL(file);
+                    }
+                  }}
+                />
+                <div className={classes.imgs}>
+                  {formik.values.image && (
+                    <img
+                      className={classes.preview}
+                      src={localStorage.getItem("listingImage")}
+                      alt="addedImage"
+                    />
+                  )}
+                  <img
+                    className={classes.addIcon}
+                    src={add}
+                    alt="add"
+                    onClick={() => document.getElementById("fileInput").click()}
+                  />
+                </div>
+              </div>
+              <ErrorMessage
+                name="image"
+                component="div"
+                className={classes.error}
+              />
+            </div>
+            <div className={classes.agent}>
+              <h2 className={classes.fieldHead}>აგენტი</h2>
+              <label htmlFor="agent">
+                აირჩიე
+                <Field as="select" name="agent" value={formik.values.agent}>
+                  <option disabled value="">
+                    აირჩიე აგენტი
+                  </option>
+                  <option>გიორგი ბრეგვაძე</option>
+                  <option>შოთა ბრეგვაძე</option>
+                  <option>ნანი ბრეგვაძე</option>
+                </Field>
+                <ErrorMessage
+                  name="agent"
+                  component="div"
+                  className={classes.error}
+                />
+              </label>
+            </div>
+            <div className={classes.buttons}>
+              <button className={classes.cancel} type="button">
+                <a href="/">გაუქმება</a>
+              </button>
+              <button className={classes.addBtn} type="submit">
+                დაამატე ლისტინგი
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default AddListing;
---
+++ src/components/pages/addListing/assets.js
@@ -0,0 +1,82 @@
+
+import * as Yup from "yup";
+export const  validationSchema = Yup.object().shape({
+    address: Yup.string()
+      .required("აუცილებელი ველი")
+      .min(2, "მინიმუმ 2 სიმბოლო"),
+    image: Yup.string()
+      .required("აუცილებელია ფოტოს ატვირთვა"),
+    postal: Yup.string()
+      .required("აუცილებელი ველი")
+      .matches(/^\d+$/, "მხოლოდ რიცხვები"),
+    price: Yup.string()
+      .required("აუცილებელი ველი")
+      .matches(/^\d+$/, "მხოლოდ რიცხვები"),
+    area: Yup.string()
+      .required("აუცილებელი ველი")
+      .matches(/^\d+$/, "მხოლოდ რიცხვები"),
+    bedrooms: Yup.string()
+      .required("აუცილებელი ველი")
+      .matches(/^[1-9]\d*$/, "მხოლოდ მთელი რიცხვები"),
+    description: Yup.string()
+      .required("აუცილებელი ველი")
+      .test(
+        "minWords",
+        "აღწერაში უნდა იყოს მინიმუმ 5 სიტყვა",
+        (value) => value && value.trim().split(/\s+/).length >= 5
+      ),
+    agent: Yup.string().required("აუცილებელი ველი"),
+    region: Yup.string().required("აუცილებელი ველი"),
+    city: Yup.string().required("აუცილებელი ველი"),
+    type: Yup.string().required("აუცილებელი ველი"),
+  });
+
+  export const initialValues = {
+    type: localStorage.getItem("type") || "",
+    address: localStorage.getItem("address") || "",
+    postal: localStorage.getItem("postal") || "",
+    region: localStorage.getItem("region") || "",
+    city: localStorage.getItem("city") || "",
+    price: localStorage.getItem("price") || "",
+    area: localStorage.getItem("area") || "",
+    bedrooms: localStorage.getItem("bedrooms") || "",
+    description: localStorage.getItem("description") || "",
+    agent: localStorage.getItem("agent") || "",
+    image: localStorage.getItem("listingImage") || "",
+    selectedRegionId:localStorage.getItem("selectedRegionId") || ""
+  };
+
+  export const fetchData = async (setRegions,setCities) => {
+    try {
+      const regions = await fetch('https://api.real-estate-manager.redberryinternship.ge/api/regions');
+      const cities = await fetch('https://api.real-estate-manager.redberryinternship.ge/api/cities');
+
+      const regionsData = await regions.json(); 
+      const cityData = await cities.json();
+      setRegions(regionsData)
+      setCities(cityData)
+    } catch (error) {
+      console.error('Error fetching data:', error); 
+    }
+  };
+
+  export const submitForm =(errors)=>{
+    console.log(errors);
+  }
+
+  export const changeHandler = (e, url)=>{
+    if(e.target.value){
+      if(e.target.name==='listingImage'){
+        console.log(url);
+        localStorage.setItem('listingImage',url)
+      }else if(e.target.name==='region'){
+        localStorage.setItem(e.target.name,e.target.value)
+        localStorage.setItem("selectedRegionId",e.target.selectedOptions[0].id)
+      }   else{
+
+       localStorage.setItem(e.target.name,e.target.value)
+      }
+    }else{
+      localStorage.removeItem(e.target.name);
+    }
+  }
